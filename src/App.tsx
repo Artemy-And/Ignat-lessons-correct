80e41adede5424e25f4090a85a7f263e53da7bf5
Artemy-And <andruschak.artemy@gmail.com>
2020-06-03 18:32:09 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Hello from "./Hello"
+
+
+const App = () => {
+  return (
+    <Hello name="Artemy" surname="Andruschak" />
+  )
+};
+
+export default App;
---
+++ src/Hello.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import classes from "./Hello.module.css";
+
+
+
+
+type NameType = {
+    name: string
+    surname: string
+}
+
+function Hello(props: NameType) {
+    return (
+
+        <div>
+            <div className={classes.container}>
+                <div className={classes.messageblue}>
+                    <h4 className={classes.messagecontent}>{props.name} {props.surname}</h4>
+
+                    <p>How is it going</p>
+                    <div className={classes.messagetimestampleft}>17:15</div>
+
+
+                </div>
+
+            </div>
+            <img src="https://cdn.ananasposter.ru/image/cache/catalog/poster/film/99/1333-1000x830.jpg"/>
+
+        </div>
+
+    )
+
+
+}
+
+
+export default Hello;
