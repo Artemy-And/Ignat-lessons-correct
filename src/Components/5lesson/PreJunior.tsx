962f885055020f7ae77d8295bb471060b20d2bef
Artemy-And <andruschak.artemy@gmail.com>
2020-07-13 17:32:29 +0300

    changed lesson 7

--- src/Components/5lesson/PreJunior.tsx
+++ src/Components/5lesson/PreJunior.tsx
@@ -4,7 +4,6 @@ import {v1} from "uuid";
 import Radio from "../6-7lesson/Common/Radio";
 
 
-
 function PreJunior(props: any) {
 
     let [items, setItems] = useState([
@@ -17,27 +16,93 @@ function PreJunior(props: any) {
 
     const [currentValue, setCurrentValue] = useState();
 
-    let [itemRadio, setItemRadio]= useState([
-        {value:"male", name:"John"},
-        {value:"male", name:"Richard"},
-        {value:"female", name:"Volerma"},
-        {value:"female", name:"Insignia"},
+    let [itemRadio, setItemRadio] = useState([
+        {value: "male", name: "John", checked: false},
+        {value: "male", name: "Bendjamin", checked: false},
+        {value: "female", name: "Volerma", checked: true},
+        {value: "female", name: "Insignia", checked: false},
 
     ])
-    let [radioCurrentValue, setRadioCurrentValue] = useState()
+    // let [radioCurrentValue, setRadioCurrentValue] = useState<boolean>()
+
+    const functionRadioChecked = (name: string) => {
+        const newItem = itemRadio.map(item => {
+            if (item.name === name) {
+                return {...item, checked: true}
+            } else {
+                return {...item, checked: false}
+            }
+        })
+        setItemRadio(newItem);
+    }
 
 
     console.log(currentValue)
-    console.log(radioCurrentValue)
-
 
 
     return (<div>
             <h1>{props.message}</h1>
-            <Select items={items} value={currentValue} setCurrentValue={setCurrentValue} />
-            <Radio itemRadio={itemRadio} value = {radioCurrentValue} setRadioCurrentValue={setRadioCurrentValue}/>
+            <Select items={items} value={currentValue} setCurrentValue={setCurrentValue}/>
+            <Radio itemRadio={itemRadio} setRadioCurrentValue={functionRadioChecked}/>
         </div>
     )
 }
 
 export default PreJunior;
+
+
+//
+// import React, {useState} from "react";
+// import Select from "../6-7lesson/Common/Select";
+// import {v1} from "uuid";
+// import Radio from "../6-7lesson/Common/Radio";
+//
+//
+//
+// function PreJunior(props: any) {
+//
+//     let [items, setItems] = useState([
+//         {value: "Milk", label: 'Milk'},
+//         {value: "Coffee", label: 'Coffee'},
+//         {value: "Tea", label: 'Tea'},
+//         {value: "Juice", label: 'Juice'},
+//         {value: "Apples", label: 'Apples'}
+//     ])
+//
+//     const [currentValue, setCurrentValue] = useState();
+//
+//     let [itemRadio, setItemRadio]= useState([
+//         {value:"male", name:"John", checked:false},
+//         {value:"male", name:"Bendjamin",checked:false},
+//         {value:"female", name:"Volerma",checked:true},
+//         {value:"female", name:"Insignia",checked:false},
+//
+//     ])
+//     // let [radioCurrentValue, setRadioCurrentValue] = useState<boolean>()
+//
+//     const functionRadioChecked =(name: string)=>{
+//         const newItem = itemRadio.map(item => {
+//             if(item.name === name) {
+//                 return {...item, checked: true}
+//             } else {
+//                 return {...item, checked: false}
+//             }
+//         })
+//         setItemRadio(newItem);
+//     }
+//
+//
+//     console.log(currentValue)
+//
+//
+//
+//
+//     return (<div>
+//             <h1>{props.message}</h1>
+//             <Select items={items} value={currentValue} setCurrentValue={setCurrentValue} />
+//             <Radio itemRadio={itemRadio} setRadioCurrentValue={functionRadioChecked} />
+//         </div>
+//     )
+// }
+//
+// export default PreJunior;
--- src/Components/6-7lesson/Common/Radio.tsx
+++ src/Components/6-7lesson/Common/Radio.tsx
@@ -1,35 +1,33 @@
 import React, {ChangeEvent, FormEvent} from "react";
 
 type radioType = {
-    value: string,
+    value: string
     name: string
+    checked: boolean
 }
 type allRadioType = {
     itemRadio: Array<radioType>
-    setRadioCurrentValue: (value: string) => void
-    value: string
+    setRadioCurrentValue: (name: string) => void
+
 }
 
 
 function Radio(props: allRadioType) {
 
-
-    const onChangeFunct = (e: any) => {//вот тут когда ставлю тип e:ChangeEvent<HTMLInputElement> не работает VALUE ниже
-        props.setRadioCurrentValue(e.currentTarget.value)//это value не работае
-    }
-
     return (
         <div><label>
+
             {props.itemRadio.map(el => {
                 return <label key={el.value}>
-                    <input value={props.value}
-                           onChange={onChangeFunct}
+                    <input checked={el.checked}
+                           onChange={() => props.setRadioCurrentValue(el.name)}
                            type="radio">
 
                     </input>{el.name}</label>
             })}
 
         </label></div>
+
     )
 
 
